b2ff62d6eef9959c9d8c3eea1c9bd759ae9ae61e
TanishJalan <108969446+TanishJalan@users.noreply.github.com>
2023-03-31 19:15:43 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -49,13 +49,18 @@ function setup() {
 function draw() {
   if(draw_apple == "set")
   {
-    document.getElementById("status").innerHTML = to_number + " Apples drawn";
-    draw_apple = "";
+
+
     for(var i = 1; i <= to_number; i++){
       x=Math.floor(Math.random() * 700);
       y=Math.floor(Math.random() * 400);
       image(apple, x, y, 50, 50);
+
     }
+    document.getElementById("status").innerHTML = to_number+"Apples drawn";
+    speak_data = to_number+"Apples drawn";
+    speak();
+    draw_apple = "";
   }
 }
 
